7f7593333ae81c02ea5bd48171e7eed4f1e4df07
hlamnguyen <lam421996@gmail.com>
2021-04-28 16:12:58 +0700

    Fix md5

--- src/common/hash.js
+++ src/common/hash.js
@@ -1,12 +1,17 @@
-"use strict";
-const nodemailer = require("nodemailer");
+const bcrypt = require('bcrypt');
+const objectHash = require('object-hash');
 
-class EmailService {
+class HashService {
     constructor() {
     }
 
-    async sendMail(length) {
+    async genSalt(length) {
+        return await bcrypt.genSalt(length);
+    }
+
+    hashMD5(data) {
+        return objectHash(data, { algorithm: 'md5', encoding: 'base64' });
     }
 }
 
-module.exports.HashService = new EmailService();
+module.exports.HashService = new HashService();
